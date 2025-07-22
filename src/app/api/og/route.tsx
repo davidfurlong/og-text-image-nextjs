eaac17333f38eb007c07b727ead4478403c4b480
David Furlong <dvfurlong@googlemail.com>
2025-07-22 14:56:46 +0200

    fix any

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -1,34 +1,34 @@
-import { ImageResponse } from 'next/og'
- 
-export const runtime = 'edge'
- 
+import { ImageResponse } from "next/og";
+
+export const runtime = "edge";
+
 export async function GET(request: Request) {
   try {
-    const { searchParams } = new URL(request.url)
-    const text = searchParams.get('text') || 'Hello World'
- 
+    const { searchParams } = new URL(request.url);
+    const text = searchParams.get("text") || "Hello World";
+
     return new ImageResponse(
       (
         <div
           style={{
-            height: '100%',
-            width: '100%',
-            display: 'flex',
-            textAlign: 'center',
-            alignItems: 'center',
-            justifyContent: 'center',
-            flexDirection: 'column',
-            flexWrap: 'nowrap',
-            backgroundColor: 'white',
-            color: 'black',
+            height: "100%",
+            width: "100%",
+            display: "flex",
+            textAlign: "center",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+            flexWrap: "nowrap",
+            backgroundColor: "white",
+            color: "black",
           }}
         >
           <div
             style={{
               fontSize: 60,
-              fontWeight: 'bold',
-              maxWidth: '90%',
-              wordWrap: 'break-word',
+              fontWeight: "bold",
+              maxWidth: "90%",
+              wordWrap: "break-word",
             }}
           >
             {text}
@@ -39,11 +39,16 @@ export async function GET(request: Request) {
         width: 1200,
         height: 630,
       }
-    )
-  } catch (e: any) {
-    console.log(`${e.message}`)
+    );
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      console.log(`${e.message}`);
+      return new Response(`Failed to generate the image`, {
+        status: 500,
+      });
+    }
     return new Response(`Failed to generate the image`, {
       status: 500,
-    })
+    });
   }
 }
